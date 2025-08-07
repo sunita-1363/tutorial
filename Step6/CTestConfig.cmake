48ca95a98d4256b053a047ab8cd5638249faaceb
sunita-1363 <sunita136333@gmail.com>
2025-08-07 13:50:18 +0700

    Step6

--- Step6/CTestConfig.cmake
+++ Step6/CTestConfig.cmake
@@ -1,3 +1,5 @@
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_SUBMIT_URL "https://my.cdash.org/submit.php?project=CMakeTutorial")
+
+set(CTEST_SOURCE_DIRECTORY "C:/sunita/dev/tutorial/Step6")
